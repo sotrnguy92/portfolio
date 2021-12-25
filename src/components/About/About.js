c3770d1f50aa9a8fbd4836cd045636e623edf9dd
sotrnguy92 <sonnguyen092@gmail.com>
2021-12-24 20:13:41 -0800

    adding icon folder and formatting about me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,23 @@ import './About.css'
 export const About = () => {
     return (
         <div className='about-container'>
-            About
+            <div className='hero-box'>
+                <h1>
+                    HI, I'M SON
+                </h1>
+                <h4>
+                    Web Developer
+                </h4>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="profile-image"/>
+                </div>
+
+
+            </div>
+            <div className='about-blurb'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dolores eius explicabo ipsa ipsum minima molestiae odit pariatur quaerat. Corporis culpa delectus earum fugit id, iure magnam saepe voluptatum? Magnam?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus excepturi impedit, laborum nemo repellat sed tempore. Consequatur dolores eveniet fuga ratione recusandae sapiente suscipit voluptatibus! Cumque eaque eligendi nam veritatis.
+            </div>
+
         </div>
     )
 }
