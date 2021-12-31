ae33751cb707e433dc0591564a3ca41c03c75dc5
sotrnguy92 <sonnguyen092@gmail.com>
2021-12-31 01:18:42 -0800

    adding minor changes to styling of projects

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,9 +7,9 @@ export const Contact = () => {
         <div className="contact-container contact">
             <PaperPlaneSvg/>
             <h1>GET IN TOUCH</h1>
-            <body className='contact-body'>
+            <div className='contact-body'>
             Want to work on a project together or just nerd out? <br/> Shoot me an email and let's make it happen!
-            </body>
+            </div>
             <button className="contact-button" onClick={()=>{window.location.href=`mailto:sonnguyen092@gmail.com`}}>Say Hi</button>
         </div>
     )
